2ed4f531cd75688f9e6c9606ba8a8302b0efe7bf
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-04-19 11:39:28 +0200

    More syso

--- src/main/java/de/difuture/ekut/pht/train/controller/controller/RouteEventController.java
+++ src/main/java/de/difuture/ekut/pht/train/controller/controller/RouteEventController.java
@@ -37,11 +37,14 @@ public class RouteEventController {
         // Save the trainAvailable message as RouteEvent
         final RouteEvent event = new RouteEvent(trainAvailable.getTag(), trainAvailable.getTrainID());
         System.out.println("SAVED as " + this.routeEventRepository.save(event).getId());
+
+        System.out.print("RouteEvent Repository has in Sink" + this.routeEventRepository.count());
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public Iterable<RouteEvent> getAll() {
 
+        System.out.print("RouteEvent Repository has in Controller" + this.routeEventRepository.count());
         return this.routeEventRepository.findAll();
     }
 }
