97daca02d3aa7afa532d910cfa2cb94af7a0ab00
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-04-19 11:21:54 +0200

    [REFACTOR] Move Sink listener to RouteEventController

--- src/main/java/de/difuture/ekut/pht/train/controller/TrainControllerApplication.java
+++ src/main/java/de/difuture/ekut/pht/train/controller/TrainControllerApplication.java
@@ -1,42 +1,15 @@
 package de.difuture.ekut.pht.train.controller;
 
-import de.difuture.ekut.pht.lib.core.messages.TrainAvailable;
-import de.difuture.ekut.pht.train.controller.repository.RouteEvent;
-import de.difuture.ekut.pht.train.controller.repository.RouteEventRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 
 @SpringBootApplication
-@EnableBinding(Sink.class)
 @EnableNeo4jRepositories("de.difuture.ekut.pht.train.controller.repository")
 public class TrainControllerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainControllerApplication.class, args);
-	}
-
-	private final RouteEventRepository routeEventRepository;
-
-	@Autowired
-	public TrainControllerApplication(RouteEventRepository routeEventRepository) {
-
-		this.routeEventRepository = routeEventRepository;
-	}
-
-	@StreamListener(target=Sink.INPUT)
-	public void sink(TrainAvailable trainAvailable) {
-
-		System.out.println(trainAvailable);
-		// Save the trainAvailable message as RouteEvent
-		this.routeEventRepository.save(
-				new RouteEvent(
-						trainAvailable.getTag(),
-						trainAvailable.getTrainID()));
 	}
 }
--- src/main/java/de/difuture/ekut/pht/train/controller/controller/RouteEventController.java
+++ src/main/java/de/difuture/ekut/pht/train/controller/controller/RouteEventController.java
@@ -4,6 +4,7 @@ import de.difuture.ekut.pht.lib.core.messages.TrainAvailable;
 import de.difuture.ekut.pht.train.controller.repository.RouteEvent;
 import de.difuture.ekut.pht.train.controller.repository.RouteEventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Lukas Zimmermann
  */
 @RestController
+@EnableBinding(Sink.class)
 @RequestMapping("/routeevent")
 public class RouteEventController {
 
@@ -26,6 +28,17 @@ public class RouteEventController {
     public RouteEventController(RouteEventRepository routeEventRepository) {
 
         this.routeEventRepository = routeEventRepository;
+    }
+
+    @StreamListener(target=Sink.INPUT)
+    public void sink(TrainAvailable trainAvailable) {
+
+        System.out.println(trainAvailable);
+        // Save the trainAvailable message as RouteEvent
+        this.routeEventRepository.save(
+                new RouteEvent(
+                        trainAvailable.getTag(),
+                        trainAvailable.getTrainID()));
     }
 
     @RequestMapping(method = RequestMethod.GET)
