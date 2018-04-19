186a4df40bbb68be033f77f2d71d52c5dc433f71
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-04-19 19:54:14 +0200

    [FIX] Wrong TransactionManager

--- src/main/java/de/difuture/ekut/pht/train/controller/controller/RouteEventController.java
+++ src/main/java/de/difuture/ekut/pht/train/controller/controller/RouteEventController.java
@@ -1,8 +1,14 @@
 package de.difuture.ekut.pht.train.controller.controller;
 
+import de.difuture.ekut.pht.lib.core.messages.TrainAvailable;
 import de.difuture.ekut.pht.train.controller.repository.routeevent.RouteEvent;
 import de.difuture.ekut.pht.train.controller.repository.routeevent.RouteEventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Lukas Zimmermann
  */
 @RestController
+@EnableBinding(Sink.class)
 @RequestMapping("/routeevent")
 public class RouteEventController {
 
@@ -23,6 +30,19 @@ public class RouteEventController {
     public RouteEventController(RouteEventRepository routeEventRepository) {
 
         this.routeEventRepository = routeEventRepository;
+    }
+
+    @StreamListener(target=Sink.INPUT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void sink(TrainAvailable trainAvailable) {
+
+        System.out.println(trainAvailable);
+        // Save the trainAvailable message as RouteEvent
+        final RouteEvent event = new RouteEvent();
+        System.out.println("SAVED as " + this.routeEventRepository.saveAndFlush(event).getId());
+
+
+        System.out.println("RouteEvent Repository has in Sink" + this.routeEventRepository.count());
     }
 
     @RequestMapping(method = RequestMethod.GET)
