3a862d674e74afc5e5d1705a9b6658470e36ae84
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-04-19 20:11:04 +0200

    [REFACTOR] Make JPA Repos explicit

--- src/main/java/de/difuture/ekut/pht/train/controller/TrainControllerApplication.java
+++ src/main/java/de/difuture/ekut/pht/train/controller/TrainControllerApplication.java
@@ -2,11 +2,15 @@ package de.difuture.ekut.pht.train.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 
 @SpringBootApplication
 @EnableNeo4jRepositories("de.difuture.ekut.pht.train.controller.repository.traindestination")
+@EnableJpaRepositories(basePackages = {
+        "de.difuture.ekut.pht.train.controller.repository.routeevent",
+        "de.difuture.ekut.pht.train.controller.repository.station"})
 public class TrainControllerApplication {
 
 	public static void main(String[] args) {
